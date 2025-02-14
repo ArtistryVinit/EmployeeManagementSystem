65f28585a60c7fdaeee74ecfed3e4f2077b34331
vinit <vinitahir2000@gmail.com>
2025-02-14 15:18:09 +0530

    update-code

--- EmployeeManagementSystem/obj/Debug/net9.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ EmployeeManagementSystem/obj/Debug/net9.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755f638500060b51d28541d647c9e2318afab1e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c913197fa7ec028ac0c85383bceda243f36e8b31")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
