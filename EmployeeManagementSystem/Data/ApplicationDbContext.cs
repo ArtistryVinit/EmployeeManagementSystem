933e7659596d4f0c15890f9910d408e70edbd02a
vinit <vinitahir2000@gmail.com>
2025-02-14 18:41:09 +0530

    code-update

--- EmployeeManagementSystem/Data/ApplicationDbContext.cs
+++ EmployeeManagementSystem/Data/ApplicationDbContext.cs
@@ -58,5 +58,68 @@ namespace EmployeeManagementSystem.Data
 
         public DbSet<SystemProfile> systemProfiles { get; set; }
 
+        public DbSet<Audit> auditLogs { get; set; }
+
+        public virtual async Task<int> SaveChangesAsync(string userId = null)
+        {
+            OnBeforeSavingChanges(userId);
+            var result = await base.SaveChangesAsync();
+            return result;
+        }
+
+        private void OnBeforeSavingChanges(string userId)
+        {
+            ChangeTracker.DetectChanges();
+            var auditEntries = new List<AuditEntry>();
+            foreach(var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is Audit || entry.State == EntityState.Detached || entry.State == EntityState.Unchanged) continue;
+
+                var auditEntry = new AuditEntry(entry);
+                auditEntry.TableName = entry.Entity.GetType().Name;
+                auditEntry.UserId = userId;
+                auditEntries.Add(auditEntry);
+
+                foreach( var property in entry.Properties)
+                {
+                    string propertyName = property.Metadata.Name;
+                    if(property.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.KeyValues[propertyName] = property.CurrentValue;
+                        continue;
+                    }
+                    switch(entry.State)
+                    {
+                        case EntityState.Added:
+                            auditEntry.AuditType = AuditType.Create;
+                            auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            break;
+
+                        case EntityState.Deleted:
+                            auditEntry.AuditType = AuditType.Delete;
+                            auditEntry.OldValues[propertyName] = property.CurrentValue;
+                            break;
+
+                        case EntityState.Modified:
+                            if(property.IsModified) 
+                            {
+                                auditEntry.ChangedColumns.Add(propertyName);
+                                auditEntry.AuditType = AuditType.Update;
+                                auditEntry.OldValues[propertyName] = property.OriginalValue;
+                                auditEntry.NewValues[propertyName] = property.CurrentValue;
+                            }
+                            
+                            break;
+                    }
+                }
+                  
+            }
+
+            foreach ( var auditentry in auditEntries )
+            {
+                auditLogs.Add(auditentry.ToAudit());
+            }
+        }
+
     }
 }
--- EmployeeManagementSystem/obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ EmployeeManagementSystem/obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f28585a60c7fdaeee74ecfed3e4f2077b34331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2064b0697dd0272bba0a7072b9c757e1e319557")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
